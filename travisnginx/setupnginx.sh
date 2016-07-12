bb6723063e1f6db146159b33e9771241176c7cca
Mihai Emil Andronache <amihaiemil@gmail.com>
2016-07-12 14:17:05 +0300

    Update setupnginx.sh
    remove trailing n

--- travisnginx/setupnginx.sh
+++ travisnginx/setupnginx.sh
@@ -5,7 +5,7 @@ set -x
 
 DIR=$(realpath $(dirname "$0"))
 
-printf "jeff:$(openssl passwd -crypt s3cr3t)n" > $DIR/passwords
+printf "jeff:$(openssl passwd -crypt s3cr3t)" > $DIR/passwords
 
 # Start nginx.
 nginx -c "$DIR/nginx.conf"
