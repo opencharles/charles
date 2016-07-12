9ea0f9509433cc609048500d5c683bf69c081eb8
Mihai Emil Andronache <amihaiemil@gmail.com>
2016-07-12 14:10:06 +0300

    Update setupnginx.sh
    passwords path

--- travisnginx/setupnginx.sh
+++ travisnginx/setupnginx.sh
@@ -5,7 +5,7 @@ set -x
 
 DIR=$(realpath $(dirname "$0"))
 
-printf "jeff:$(openssl passwd -crypt s3cr3t)n" > passwords
+printf "jeff:$(openssl passwd -crypt s3cr3t)n" > $DIR/passwords
 
 # Start nginx.
 nginx -c "$DIR/nginx.conf"
